ccd83cb09d9d9312390b4cbe80795737a33a22b0
Jekaterina <ek.olshannikova@gmail.com>
2023-10-16 11:18:13 +0200

    adding two fields and testing them

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 
 android {
     namespace = "com.example.ntptime"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ntptime"
-        minSdk = 33
+        minSdk = 34
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/example/ntptime/MainActivity.java
+++ app/src/main/java/com/example/ntptime/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.ntptime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView systemTimeTextView;
+    private TextView networkTimeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the TextView elements in the layout via ID
+        systemTimeTextView = findViewById(R.id.systemTimeTextView);
+        networkTimeTextView = findViewById(R.id.networkTimeTextView);
+
+        systemTimeTextView.setText("Systemtid: " + getSystemTime());
+        networkTimeTextView.setText("Nätverkstid: " + getNetworkTime());
     }
+
+    // function for pick up system time
+    private String getSystemTime() {
+        return "12:34:56";
+    }
+
+    // function for pick up network time
+    private String getNetworkTime() {
+        return "13:45:58";
+    }
+
 }
